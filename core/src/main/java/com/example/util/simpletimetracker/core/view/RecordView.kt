92d6f7beedc29aa49326e0354530f43e67c576e2
razeeman <freeraz@mail.ru>
2020-11-04 12:50:53 +0300

    merge layout for RecordView

--- core/src/main/java/com/example/util/simpletimetracker/core/view/RecordView.kt
+++ core/src/main/java/com/example/util/simpletimetracker/core/view/RecordView.kt
@@ -4,23 +4,34 @@ import android.content.Context
 import android.graphics.Color
 import android.util.AttributeSet
 import android.view.View
-import android.widget.FrameLayout
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import com.example.util.simpletimetracker.core.R
-import kotlinx.android.synthetic.main.record_view_layout.view.*
+import kotlinx.android.synthetic.main.record_view_layout.view.ivRecordItemIcon
+import kotlinx.android.synthetic.main.record_view_layout.view.tvRecordItemDuration
+import kotlinx.android.synthetic.main.record_view_layout.view.tvRecordItemName
+import kotlinx.android.synthetic.main.record_view_layout.view.tvRecordItemTimeFinished
+import kotlinx.android.synthetic.main.record_view_layout.view.tvRecordItemTimeStarted
 
 class RecordView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : FrameLayout(
+) : CardView(
     context,
     attrs,
     defStyleAttr
 ) {
 
     init {
-        // TODO Merge layout?
         View.inflate(context, R.layout.record_view_layout, this)
+
+        ContextCompat.getColor(context, R.color.black).let(::setCardBackgroundColor)
+        radius = resources.getDimensionPixelOffset(R.dimen.record_type_card_corner_radius).toFloat()
+        // TODO doesn't work here for some reason, need to set in the layout
+        cardElevation = resources.getDimensionPixelOffset(R.dimen.record_type_card_elevation).toFloat()
+        preventCornerOverlap = false
+        useCompatPadding = true
 
         context.obtainStyledAttributes(attrs, R.styleable.RecordView, defStyleAttr, 0)
             .run {
@@ -42,7 +53,7 @@ class RecordView @JvmOverloads constructor(
 
     var itemColor: Int = 0
         set(value) {
-            layoutRecordItem.setCardBackgroundColor(value)
+            setCardBackgroundColor(value)
             field = value
         }
 
