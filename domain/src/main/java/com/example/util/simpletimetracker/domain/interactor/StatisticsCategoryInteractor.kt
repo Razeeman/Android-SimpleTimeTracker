6d8f5403352cd9163de52764b77626c1361dcc4d
razeeman <freeraz@mail.ru>
2020-12-13 11:48:51 +0300

    fix statistics for types with several categories

--- domain/src/main/java/com/example/util/simpletimetracker/domain/interactor/StatisticsCategoryInteractor.kt
+++ domain/src/main/java/com/example/util/simpletimetracker/domain/interactor/StatisticsCategoryInteractor.kt
@@ -1,6 +1,8 @@
 package com.example.util.simpletimetracker.domain.interactor
 
 import com.example.util.simpletimetracker.domain.mapper.StatisticsMapper
+import com.example.util.simpletimetracker.domain.model.Record
+import com.example.util.simpletimetracker.domain.model.RecordTypeCategory
 import com.example.util.simpletimetracker.domain.model.StatisticsCategory
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -14,15 +16,9 @@ class StatisticsCategoryInteractor @Inject constructor(
 
     // TODO simplify mappers?
     suspend fun getAll(): List<StatisticsCategory> = withContext(Dispatchers.IO) {
-        val recordTypeCategories = recordTypeCategoryInteractor.getAll()
-            .map { it.recordTypeId to it.categoryId }.toMap()
+        val allRecords = recordInteractor.getAll()
 
-        recordInteractor.getAll()
-            .mapNotNull { record ->
-                (recordTypeCategories[record.typeId] ?: return@mapNotNull null) to record
-            }
-            .groupBy { (categoryId, _) -> categoryId }
-            .mapValues { entry -> entry.value.map { it.second } }
+        getCategoryRecords(allRecords)
             .map { (categoryId, records) ->
                 StatisticsCategory(
                     categoryId = categoryId,
@@ -31,21 +27,28 @@ class StatisticsCategoryInteractor @Inject constructor(
             }
     }
 
-    suspend fun getFromRange(start: Long, end: Long): List<StatisticsCategory> = withContext(Dispatchers.IO) {
-        val recordTypeCategories = recordTypeCategoryInteractor.getAll()
-            .map { it.recordTypeId to it.categoryId }.toMap()
+    suspend fun getFromRange(
+        start: Long,
+        end: Long
+    ): List<StatisticsCategory> = withContext(Dispatchers.IO) {
+        val allRecords = recordInteractor.getFromRange(start, end)
 
-        recordInteractor.getFromRange(start, end)
-            .mapNotNull { record ->
-                (recordTypeCategories[record.typeId] ?: return@mapNotNull null) to record
-            }
-            .groupBy { (categoryId, _) -> categoryId }
-            .mapValues { entry -> entry.value.map { it.second } }
+        getCategoryRecords(allRecords)
             .map { (categoryId, records) ->
                 StatisticsCategory(
                     categoryId = categoryId,
                     duration = statisticsMapper.mapToDurationFromRange(records, start, end)
                 )
             }
+    }
+
+    private suspend fun getCategoryRecords(allRecords: List<Record>): Map<Long, List<Record>> {
+        val recordTypeCategories = recordTypeCategoryInteractor.getAll()
+            .groupBy(RecordTypeCategory::categoryId)
+            .mapValues { it.value.map(RecordTypeCategory::recordTypeId) }
+
+        return recordTypeCategories
+            .mapValues { (_, typeIds) -> allRecords.filter { it.typeId in typeIds } }
+            .filterValues(List<Record>::isNotEmpty)
     }
 }
