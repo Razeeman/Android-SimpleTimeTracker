3b458a4a4acb0892c6ad61968a242f16f10c9025
razeeman <freeraz@mail.ru>
2021-03-13 12:27:32 +0300

    add tags and comment to csv export

--- data_local/src/main/java/com/example/util/simpletimetracker/data_local/resolver/CsvRepoImpl.kt
+++ data_local/src/main/java/com/example/util/simpletimetracker/data_local/resolver/CsvRepoImpl.kt
@@ -3,9 +3,11 @@ package com.example.util.simpletimetracker.data_local.resolver
 import android.content.ContentResolver
 import android.net.Uri
 import android.os.ParcelFileDescriptor
+import com.example.util.simpletimetracker.domain.model.Category
 import com.example.util.simpletimetracker.domain.model.Record
 import com.example.util.simpletimetracker.domain.model.RecordType
 import com.example.util.simpletimetracker.domain.repo.RecordRepo
+import com.example.util.simpletimetracker.domain.repo.RecordTypeCategoryRepo
 import com.example.util.simpletimetracker.domain.repo.RecordTypeRepo
 import com.example.util.simpletimetracker.domain.resolver.CsvRepo
 import kotlinx.coroutines.Dispatchers
@@ -20,7 +22,8 @@ import javax.inject.Inject
 class CsvRepoImpl @Inject constructor(
     private val contentResolver: ContentResolver,
     private val recordTypeRepo: RecordTypeRepo,
-    private val recordRepo: RecordRepo
+    private val recordRepo: RecordRepo,
+    private val recordTypeCategoryRepo: RecordTypeCategoryRepo
 ) : CsvRepo {
 
     override suspend fun saveCsvFile(uriString: String): CsvRepo.ResultCode = withContext(Dispatchers.IO) {
@@ -43,7 +46,9 @@ class CsvRepoImpl @Inject constructor(
             recordRepo.getAll()
                 .sortedBy { it.timeStarted }
                 .forEach { record ->
-                    toCsvString(record, recordTypes[record.typeId])
+                    val categories = recordTypeCategoryRepo.getCategoriesByType(record.typeId)
+
+                    toCsvString(record, recordTypes[record.typeId], categories)
                         ?.toByteArray()
                         ?.let { fileOutputStream?.write(it) }
                 }
@@ -64,13 +69,15 @@ class CsvRepoImpl @Inject constructor(
         }
     }
 
-    private fun toCsvString(record: Record, recordType: RecordType?): String? {
+    private fun toCsvString(record: Record, recordType: RecordType?, tags: List<Category>): String? {
         return if (recordType != null) {
             String.format(
-                "%s,%s,%s\n",
-                recordType.name,
+                "%s,%s,%s,%s,%s\n",
+                recordType.name.clean(),
                 formatDateTime(record.timeStarted),
-                formatDateTime(record.timeEnded)
+                formatDateTime(record.timeEnded),
+                record.comment.clean(),
+                tags.takeUnless { it.isEmpty() }?.joinToString(separator = " ") { it.name.clean() }.orEmpty()
             )
         } else {
             null
@@ -83,8 +90,11 @@ class CsvRepoImpl @Inject constructor(
         }
     }
 
+    private fun String.clean() =
+        replace("[\n\t]".toRegex(), " ").replace(",", " ")
+
     companion object {
-        private const val CSV_HEADER = "activity name,time started,time ended"
+        private const val CSV_HEADER = "activity name,time started,time ended,comment,tags"
 
         private val dateTimeFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US)
     }
