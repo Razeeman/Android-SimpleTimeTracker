2912128c075cebcc4cc3bf7fd91474abef1bf489
razeeman <freeraz@mail.ru>
2021-04-04 08:41:58 +0300

    change ci

--- app/src/androidTest/java/com/example/util/simpletimetracker/di/TestAppComponent.kt
+++ app/src/androidTest/java/com/example/util/simpletimetracker/di/TestAppComponent.kt
@@ -1,18 +1,5 @@
 package com.example.util.simpletimetracker.di
 
-import com.example.util.simpletimetracker.AddRecordTest
-import com.example.util.simpletimetracker.AddRecordTypeTest
-import com.example.util.simpletimetracker.ChangeRecordTest
-import com.example.util.simpletimetracker.ChangeRecordTypeTest
-import com.example.util.simpletimetracker.DeleteRecordTest
-import com.example.util.simpletimetracker.DeleteRecordTypeTest
-import com.example.util.simpletimetracker.MainScreenEmptyTest
-import com.example.util.simpletimetracker.MainScreenTest
-import com.example.util.simpletimetracker.RecordsRangesTest
-import com.example.util.simpletimetracker.StartRecordTest
-import com.example.util.simpletimetracker.StatisticsFilterTest
-import com.example.util.simpletimetracker.StatisticsRangesTest
-import com.example.util.simpletimetracker.StatisticsTest
 import com.example.util.simpletimetracker.data_local.di.DataLocalModule
 import com.example.util.simpletimetracker.feature_notification.di.NotificationModule
 import com.example.util.simpletimetracker.feature_widget.di.WidgetModule
@@ -33,17 +20,4 @@ import javax.inject.Singleton
 interface TestAppComponent : AppComponent {
 
     fun inject(into: BaseUiTest)
-    fun inject(into: MainScreenTest)
-    fun inject(into: MainScreenEmptyTest)
-    fun inject(into: AddRecordTypeTest)
-    fun inject(into: ChangeRecordTypeTest)
-    fun inject(into: DeleteRecordTypeTest)
-    fun inject(into: AddRecordTest)
-    fun inject(into: ChangeRecordTest)
-    fun inject(into: DeleteRecordTest)
-    fun inject(into: StartRecordTest)
-    fun inject(into: RecordsRangesTest)
-    fun inject(into: StatisticsTest)
-    fun inject(into: StatisticsFilterTest)
-    fun inject(into: StatisticsRangesTest)
 }
