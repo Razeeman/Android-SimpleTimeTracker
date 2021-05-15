53939d392dd35cd66627db9bb2c8558ad93805b6
razeeman <freeraz@mail.ru>
2021-05-15 13:12:24 +0300

    add message on archive restore

--- feature_archive/src/main/java/com/example/util/simpletimetracker/feature_archive/viewModel/ArchiveViewModel.kt
+++ feature_archive/src/main/java/com/example/util/simpletimetracker/feature_archive/viewModel/ArchiveViewModel.kt
@@ -8,19 +8,24 @@ import com.example.util.simpletimetracker.core.adapter.ViewHolderType
 import com.example.util.simpletimetracker.core.adapter.category.CategoryViewData
 import com.example.util.simpletimetracker.core.adapter.loader.LoaderViewData
 import com.example.util.simpletimetracker.core.extension.set
+import com.example.util.simpletimetracker.core.repo.ResourceRepo
 import com.example.util.simpletimetracker.core.viewData.RecordTypeViewData
 import com.example.util.simpletimetracker.domain.interactor.RecordTagInteractor
 import com.example.util.simpletimetracker.domain.interactor.RecordTypeInteractor
 import com.example.util.simpletimetracker.domain.model.TagType
+import com.example.util.simpletimetracker.feature_archive.R
 import com.example.util.simpletimetracker.feature_archive.dialog.ArchiveDialogParams
 import com.example.util.simpletimetracker.feature_archive.interactor.ArchiveViewDataInteractor
+import com.example.util.simpletimetracker.navigation.Notification
 import com.example.util.simpletimetracker.navigation.Router
 import com.example.util.simpletimetracker.navigation.Screen
+import com.example.util.simpletimetracker.navigation.params.ToastParams
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class ArchiveViewModel @Inject constructor(
     private val router: Router,
+    private val resourceRepo: ResourceRepo,
     private val archiveViewDataInteractor: ArchiveViewDataInteractor,
     private val recordTypeInteractor: RecordTypeInteractor,
     private val recordTagInteractor: RecordTagInteractor
@@ -56,17 +61,24 @@ class ArchiveViewModel @Inject constructor(
 
     fun onRestoreClick(params: ArchiveDialogParams?) {
         viewModelScope.launch {
+            var message = ""
+
             when (params) {
                 is ArchiveDialogParams.Activity -> {
                     recordTypeInteractor.restore(params.id)
-                    // TODO show message
+                    message = resourceRepo.getString(R.string.archive_activity_restored)
                 }
                 is ArchiveDialogParams.RecordTag -> {
                     recordTagInteractor.restore(params.id)
-                    // TODO show message
+                    message = resourceRepo.getString(R.string.archive_tag_restored)
                 }
             }
+
             updateViewData()
+            router.show(
+                notification = Notification.TOAST,
+                data = ToastParams(message)
+            )
         }
     }
 
