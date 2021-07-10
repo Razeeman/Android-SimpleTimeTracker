d312d95128c3bf944efa2ec1b772e7109d7c59bf
razeeman <freeraz@mail.ru>
2021-07-10 10:49:41 +0300

    remove unused BaseRecyclerAdapterDelegate

--- core/src/main/java/com/example/util/simpletimetracker/core/adapter/BaseRecyclerBindingAdapterDelegate.kt
+++ core/src/main/java/com/example/util/simpletimetracker/core/adapter/BaseRecyclerBindingAdapterDelegate.kt
@@ -1,8 +1,23 @@
 package com.example.util.simpletimetracker.core.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
+
+interface RecyclerAdapterDelegate {
+    fun isForValidType(check: ViewHolderType): Boolean
+
+    fun onCreateViewHolder(parent: ViewGroup): BaseRecyclerViewHolder
+
+    fun getViewHolderTypeName(): String
+}
+
+abstract class BaseRecyclerViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    abstract fun bind(item: ViewHolderType, payloads: List<Any>)
+}
 
 inline fun <reified T : ViewHolderType, B : ViewBinding> createRecyclerBindingAdapterDelegate(
     noinline inflater: (LayoutInflater, ViewGroup?, Boolean) -> B,
