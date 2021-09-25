aea144e9584bd74ddb178485b3d6b622da26e3c6
razeeman <freeraz@mail.ru>
2021-09-25 10:59:18 +0300

    code review, move features to separate folder

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,23 +4,12 @@ include(
     "domain",
     "core",
     "navigation",
-    "data_local",
-    "feature_main",
-    "feature_running_records",
-    "feature_change_running_record",
-    "feature_change_record_type",
-    "feature_records",
-    "feature_records_all",
-    "feature_change_record",
-    "feature_statistics",
-    "feature_statistics_detail",
-    "feature_dialogs",
-    "feature_settings",
-    "feature_widget",
-    "feature_notification",
-    "feature_categories",
-    "feature_change_category",
-    "feature_change_record_tag",
-    "feature_archive",
-    "feature_tag_selection"
+    "data_local"
 )
+
+file("features").walkTopDown().maxDepth(1).forEach { dir ->
+    if (dir.isDirectory) {
+        include(dir.name)
+        project(":${dir.name}").projectDir = dir
+    }
+}
