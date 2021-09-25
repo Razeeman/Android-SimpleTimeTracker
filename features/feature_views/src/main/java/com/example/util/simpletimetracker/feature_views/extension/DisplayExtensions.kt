ff40854dbcedd95c0599a8146966cba1b30b3c96
razeeman <freeraz@mail.ru>
2021-09-25 23:58:47 +0300

    fix display extensions

--- features/feature_views/src/main/java/com/example/util/simpletimetracker/feature_views/extension/DisplayExtensions.kt
+++ features/feature_views/src/main/java/com/example/util/simpletimetracker/feature_views/extension/DisplayExtensions.kt
@@ -10,9 +10,9 @@ fun Float.dpToPx(): Int = TypedValue.applyDimension(COMPLEX_UNIT_DIP, this, getD
 
 fun Int.dpToPx(): Int = this.toFloat().dpToPx()
 
-fun Float.pxToDp(): Int = (this / Resources.getSystem().displayMetrics.density).toInt()
+fun Float.pxToDp(): Int = (this / Resources.getSystem().displayMetrics.density).roundToInt()
 
-fun Int.pxToDp(): Int = this.toFloat().toInt()
+fun Int.pxToDp(): Int = this.toFloat().pxToDp()
 
 fun Float.spToPx(): Int = TypedValue.applyDimension(COMPLEX_UNIT_SP, this, getDisplayMetrics()).roundToInt()
 
