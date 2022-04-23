c42f324929dd7db4aeb39a88df136f1491620eaa
razeeman <freeraz@mail.ru>
2022-04-23 18:49:37 +0300

    release 1.18

--- buildSrc/src/main/kotlin/com/example/util/simpletimetracker/Base.kt
+++ buildSrc/src/main/kotlin/com/example/util/simpletimetracker/Base.kt
@@ -1,8 +1,8 @@
 package com.example.util.simpletimetracker
 
 object Base {
-    const val versionCode = 18
-    const val versionName = "1.17"
+    const val versionCode = 19
+    const val versionName = "1.18"
     const val minSDK = 19
     const val currentSDK = 30
 }
