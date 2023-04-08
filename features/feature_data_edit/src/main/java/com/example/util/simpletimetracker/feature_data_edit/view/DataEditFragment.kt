69b0a095902ed341374b0dae3d6ebfc8a771b792
razeeman <freeraz@mail.ru>
2023-04-08 09:58:35 +0300

    adding data edit content

--- features/feature_data_edit/src/main/java/com/example/util/simpletimetracker/feature_data_edit/view/DataEditFragment.kt
+++ features/feature_data_edit/src/main/java/com/example/util/simpletimetracker/feature_data_edit/view/DataEditFragment.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.example.util.simpletimetracker.core.base.BaseFragment
 import com.example.util.simpletimetracker.feature_data_edit.viewModel.DataEditViewModel
+import com.example.util.simpletimetracker.feature_views.extension.setOnClick
 import dagger.hilt.android.AndroidEntryPoint
 import com.example.util.simpletimetracker.feature_data_edit.databinding.DataEditFragmentBinding as Binding
 
@@ -17,10 +18,12 @@ class DataEditFragment : BaseFragment<Binding>() {
     private val viewModel: DataEditViewModel by viewModels()
 
     override fun initUi(): Unit = with(binding) {
-        // TODO
+        btnDataEditSelectRecords.setOnClick(throttle(viewModel::onSelectRecordsClick))
     }
 
     override fun initViewModel(): Unit = with(viewModel) {
-        // TODO
+        with(binding) {
+            selectedRecordsCountViewData.observe(tvDataEditSelectedRecords::setText)
+        }
     }
 }
--- features/feature_data_edit/src/main/java/com/example/util/simpletimetracker/feature_data_edit/viewModel/DataEditViewModel.kt
+++ features/feature_data_edit/src/main/java/com/example/util/simpletimetracker/feature_data_edit/viewModel/DataEditViewModel.kt
@@ -1,6 +1,10 @@
 package com.example.util.simpletimetracker.feature_data_edit.viewModel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.util.simpletimetracker.core.repo.ResourceRepo
+import com.example.util.simpletimetracker.feature_data_edit.R
 import com.example.util.simpletimetracker.navigation.Router
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -8,7 +12,24 @@ import javax.inject.Inject
 @HiltViewModel
 class DataEditViewModel @Inject constructor(
     private val router: Router,
+    private val resourceRepo: ResourceRepo,
 ) : ViewModel() {
 
+    val selectedRecordsCountViewData: LiveData<String> =
+        MutableLiveData(loadSelectedRecordsCountViewData())
 
+    private var selectedRecordsCount: Int = 0
+
+    fun onSelectRecordsClick() {
+        // TODO
+    }
+
+    private fun loadSelectedRecordsCountViewData(): String {
+        val recordsString = resourceRepo.getQuantityString(
+            R.plurals.statistics_detail_times_tracked,
+            selectedRecordsCount
+        ).lowercase()
+
+        return "$selectedRecordsCount $recordsString"
+    }
 }
