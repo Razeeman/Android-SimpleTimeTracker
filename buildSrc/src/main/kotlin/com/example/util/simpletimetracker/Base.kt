10c5a32c622d00f25f2e6d6bee407d7c7fe4be3d
razeeman <freeraz@mail.ru>
2023-05-21 18:59:54 +0300

    raise minSdk, disable png generation, fix some icons for lower apis

--- buildSrc/src/main/kotlin/com/example/util/simpletimetracker/Base.kt
+++ buildSrc/src/main/kotlin/com/example/util/simpletimetracker/Base.kt
@@ -3,7 +3,7 @@ package com.example.util.simpletimetracker
 object Base {
     const val versionCode = 26
     const val versionName = "1.25"
-    const val minSDK = 19
+    const val minSDK = 21
     const val currentSDK = 32
     // TODO after raising sdk to 33 check alarmManager scheduling.
 }
