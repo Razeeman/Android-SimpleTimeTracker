e37cbb73e8d7df2c153868a677c7e1b0a1229dd3
razeeman <freeraz@mail.ru>
2023-06-18 12:44:10 +0300

    release 1.27

--- buildSrc/src/main/kotlin/com/example/util/simpletimetracker/Base.kt
+++ buildSrc/src/main/kotlin/com/example/util/simpletimetracker/Base.kt
@@ -1,8 +1,8 @@
 package com.example.util.simpletimetracker
 
 object Base {
-    const val versionCode = 27
-    const val versionName = "1.26"
+    const val versionCode = 28
+    const val versionName = "1.27"
     const val minSDK = 21
     const val currentSDK = 32
     // TODO after raising sdk to 33 check alarmManager scheduling.
