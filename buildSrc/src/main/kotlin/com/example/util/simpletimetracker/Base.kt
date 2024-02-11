5b0d773588b392f6883002db758434abe8d3d55a
razeeman <freeraz@mail.ru>
2024-02-11 19:21:19 +0300

    release 1.37

--- buildSrc/src/main/kotlin/com/example/util/simpletimetracker/Base.kt
+++ buildSrc/src/main/kotlin/com/example/util/simpletimetracker/Base.kt
@@ -4,8 +4,8 @@ object Base {
     const val applicationId = "com.razeeman.util.simpletimetracker"
     const val namespace = "com.example.util.simpletimetracker"
 
-    const val versionCode = 37
-    const val versionName = "1.36"
+    const val versionCode = 38
+    const val versionName = "1.37"
     const val minSDK = 21
     const val currentSDK = 34
 }
