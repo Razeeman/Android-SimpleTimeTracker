63269965ea68db1b33c29f8e964386069ab5e11e
razeeman <freeraz@mail.ru>
2024-03-16 12:11:13 +0300

    change wear versioning

--- buildSrc/src/main/kotlin/com/example/util/simpletimetracker/Base.kt
+++ buildSrc/src/main/kotlin/com/example/util/simpletimetracker/Base.kt
@@ -4,13 +4,14 @@ object Base {
     const val applicationId = "com.razeeman.util.simpletimetracker"
     const val namespace = "com.example.util.simpletimetracker"
 
+    // Raise by 2 to account for wear version code.
     const val versionCode = 39
     const val versionName = "1.38"
     const val minSDK = 21
     const val currentSDK = 34
 
-    const val versionCodeWear = 40
-    const val versionNameWear = "1.0"
+    const val versionCodeWear = versionCode + 1
+    const val versionNameWear = versionName + "w"
     const val minSDKWear = 26
     const val currentSDKWear = currentSDK
     const val targetSDKWear = 33
